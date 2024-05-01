c69af795c2ff38e839e64a809e4a0771ccaa074c
luh <lucineia.rsilvah@gmail.com>
2024-05-01 19:59:14 -0300

    adicinando titulo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ function Footer(){
     <>
     <footer className='rodape'>
      <p>&copy;Só amores petshop - todos os direitos reservados</p>
-     <p>Desenvolvido por : </p> <a target='_blank' href='https://github.com/LucineiaSilvah'>Lucineia Silvah ^.^ 2024.</a>
+     <p>Desenvolvido por : </p> <a target='_blank' href='https://github.com/LucineiaSilvah' rel="noreferrer">Lucineia Silvah ^.^ 2024.</a>
     </footer>
     </>
   )
