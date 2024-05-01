01f372fe98ebe4dba5628c4854385c64525c9ea1
luh <lucineia.rsilvah@gmail.com>
2024-05-01 19:20:24 -0300

    ajustando detalhes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
 import '../Header/Header.css'
 import NavBar from "../Nav/Nav";
+
 function Header(){
   return (
    <div>
      <header className="cabecalho">
+    
      <h1>Só A<i class="fa-solid fa-paw"></i>ores</h1>
       <NavBar></NavBar>
      </header>
