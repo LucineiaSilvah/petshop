7f763adb511c9d4bf0cb20e21178584476eaf23b
luh <lucineia.rsilvah@gmail.com>
2024-05-05 16:00:17 -0300

    adicionando menu hamburguer

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,22 +1,33 @@
-import './Nav.css';
-
-function NavBar(){
-  return(
-    <>
-    <nav className='nav-bar'>
-      <ul>
-        <li>
-          <a href="#services">Serviços</a>
-        </li>
-        <li>
-          <a href="#sobre">Sobre</a>
-        </li>
-        <li>
-          <a href="#contact">Contato</a>
-        </li>
-      </ul>
-    </nav>
-    </>
-  )
+import "./Nav.css";
+import { useState } from "react";
+function NavBar() {
+  const [active, setMode] = useState(false);
+  const ToggleMode = () => {
+    setMode(!active);
+  };
+  return (
+    <div className="Menu">
+      <div className="menuIcon">
+      <div className={active ? "icon iconActive" : "icon"} onClick={ToggleMode}>
+        <div className="hamburguer hamburguerIcon"> </div>
+      </div>
+      </div>
+      <div className={active ? "menu menuOpen" : "menu menuClose"}>
+        <nav className="list">
+          <ul className="listItems">
+            <li>
+              <a href="#services">Serviços</a>
+            </li>
+            <li>
+              <a href="#sobre">Sobre</a>
+            </li>
+            <li>
+              <a href="#contact">Contato</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 }
 export default NavBar;
